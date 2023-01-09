9c9c18c2fc1e99fd5ad89a5b18a5037b29e6946f
Sahil Garg <sahil.garg78272026@gmail.com>
2023-01-09 23:31:57 +0530

    files getting upload

--- lead_management/csvupload/views.py
+++ lead_management/csvupload/views.py
@@ -18,12 +18,9 @@ def home(request):
 
     if request.method=='POST':
         file2 = request.FILES['file']
-        path = os.path.join(settings.BASE_DIR,'media') + str(file2)
-        print(path)
-        df = pd.read_csv(path)
-        print(df)
         document = FileUpload.objects.create(file=file2)
         document.save()
-        print('read : ',file2)
-        return HttpResponse('Files saved Succesfully')
+        path = os.path.join(settings.BASE_DIR,'media') + '\\' + str(file2)
+        df = pd.read_csv(path)
+        return render(request,'index2.html',{'data': df.to_dict(orient='records')})
     return render(request,'index.html')
