052fe92a5516c82cffe6f7dea6f46eb9026a5698
Sahil Garg <sahil.garg78272026@gmail.com>
2023-01-09 23:53:54 +0530

    time range added

--- lead_management/csvupload/views.py
+++ lead_management/csvupload/views.py
@@ -1,26 +1,24 @@
 from django.shortcuts import render
-from .forms import CSVUploadForm
 from django.http import HttpRequest,HttpResponse
 from .models import *
 import pandas as pd
 from django.conf import settings
 import os
-
 import csv
+from datetime import datetime
 
 def home(request):
-    
-    print('request.scheme : ' , request.scheme) # showing the type of protocol, is it http or https
-  # print('request.body : ' ,request.body)
-    print('request.POST : ' ,request.POST)
-    print('request.path : ' ,request.path)
-    print('request.content_type : ' ,request.content_type)
-
     if request.method=='POST':
         file2 = request.FILES['file']
         document = FileUpload.objects.create(file=file2)
         document.save()
         path = os.path.join(settings.BASE_DIR,'media') + '\\' + str(file2)
         df = pd.read_csv(path)
-        return render(request,'index2.html',{'data': df.to_dict(orient='records')})
+        now = datetime.now()
+        current_time = now.strftime("%H")
+        if int(current_time) >=9 and int(current_time) <=18:
+            return render(request,'index2.html',{'data': df.to_dict(orient='records')})
+        else:
+            return HttpResponse("Please add data between 9 to 6 only")
     return render(request,'index.html')
+
